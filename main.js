96f0b821a11c62d4e55a1a25dec17a44b977929d
Andrey1303110 <andrey1303110@gmail.com>
2021-04-08 22:34:01 +0300

    some fixes - add hp

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 const player1 = {
     name: 'Scorpio',
-    hp: 50,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['knife', 'glock'], 
     attack: function() {console.log (player1.name + 'Fight');} 
@@ -10,13 +9,12 @@ const player1 = {
 
 const player2 = {
     name: 'Sub-Zero',
-    hp: 50,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['knife', 'glock'], 
     attack: function() {console.log (player2.name + 'Fight');} 
 };
 
-function createPlayer(player, person) {
+function createPlayer(player, person, hp) {
     const $player=document.createElement('div'); 
     $player.classList.add(player);
 
@@ -31,7 +29,7 @@ function createPlayer(player, person) {
     const $life=document.createElement('div'); 
     $life.classList.add('life'); 
     $progressbar.appendChild($life);
-    $life.style.width='100%';
+    $life.style.width=hp + '%';
 
     const $name=document.createElement('div'); 
     $name.classList.add('name'); 
@@ -46,5 +44,5 @@ function createPlayer(player, person) {
     $arenas.appendChild($player);
 };
 
-createPlayer('player1', player1);
-createPlayer('player2', player2);
+createPlayer('player1', player1, 100);
+createPlayer('player2', player2, 10);
